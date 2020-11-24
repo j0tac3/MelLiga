fdf20199ccca6400b2a9376c3043013f65c40db1
unknown <j0s3.90jr@gmail.com>
2020-11-24 08:09:42 +0100

    Modificado el elemento campo

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,23 @@
 const iconoMenu = document.getElementById('icono-menu');
 const menulateral = document.getElementById('menu-lateral');
+const contenedorPrincipal = document.getElementById('contenedor-prueba');
 
 const abrirOrCerrarMenu = () => {
-    menulateral.classList.add('menu-lateral__selected')
+    let menuDesplegado = document.getElementsByClassName('menu-lateral__selected');
+    if (menuDesplegado.length > 0){
+        cerrarMenu();
+    }
+    else{
+        menulateral.classList.add('menu-lateral__selected')
+        contenedorPrincipal.classList.add('prueba-menu__selected');
+        iconoMenu.classList.add('icono-menu__selected');
+    }
 }
 const cerrarMenu = () => {
-    menulateral.classList.remove('menu-lateral__selected')
+    menulateral.classList.remove('menu-lateral__selected');
+    contenedorPrincipal.classList.remove('prueba-menu__selected');
+    iconoMenu.classList.remove('icono-menu__selected');
 }
 
 iconoMenu.addEventListener('click', abrirOrCerrarMenu);
-menulateral.addEventListener('mouseleave', cerrarMenu);
+contenedorPrincipal.addEventListener('click', cerrarMenu);
