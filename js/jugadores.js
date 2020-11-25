4e2bfbb002e70fc12d1b439f050f3152005f9e7c
Jose Cordeiro Andrade <josecordeiro.andrade@Zucchetti.es>
2020-11-25 15:46:47 +0100

    Añadida funcionalidad de ocultar/mostrar lista de Jugadores

--- js/jugadores.js
+++ js/jugadores.js
@@ -1,36 +1,16 @@
-/* const getAllJugadoresCampo = async () => {
-    return new Promise((resolve, reject) => {
-        axios({
-            method : 'GET',
-            url : 'http://127.0.0.1:8000/api/jugadores'
-        }).then(res => {
-            resolve(res.data);
-        }).catch(error => console.log(error));
-    })
-}
-const rellenarJugadoresCampo = (jugadoresToAdd) => {
-    let jugadoresCampo = document.getElementById('jugadores-campo');
-    let fragment = document.createDocumentFragment();
-    for (let jugador of jugadoresToAdd){
-        let contenedorTarjeta = document.createElement('DIV');
-        contenedorTarjeta.classList.add('contenedor-tarjeta');
-        let jugadorCard = document.createElement('DIV');
-        jugadorCard.classList.add('jugador-card');
-        let imgCard = document.createElement('IMG');
-        imgCard.classList.add('img-card-jugador');
-        imgCard.src = `../media/jugador_perfil/${jugador.nombre}.png`;
-        imgCard.onerror = obtenerSrcImagenCampo;
-        jugadorCard.appendChild(imgCard);
-        contenedorTarjeta.appendChild(jugadorCard);
-        fragment.appendChild(contenedorTarjeta);
+const tablaJugadores = document.getElementById('table-container');
+const btnLista = document.getElementById('btn-lista');
+
+const mostarOcultarTablaJugadores = () => {
+    let isTableVisible = document.getElementsByClassName('table-container__oculta');
+    if (isTableVisible.length > 0){
+        tablaJugadores.classList.replace('table-container__oculta', 'table-container__visible');
+        btnLista.textContent = "Ocultar Lista de Jugadores";
+    }   
+    else {
+        tablaJugadores.classList.replace('table-container__visible', 'table-container__oculta');
+        btnLista.textContent = "Ver Lista de Jugadores";
     }
-    jugadoresCampo.appendChild(fragment);
 }
-const obtenerSrcImagenCampo = (e) => {
-    e.target.src = '../media/img-not-found.png';
-}
-const cargarJugadoresCampo = async () => {
-    let jugadoresCampo = await getAllJugadoresCampo();
-    rellenarJugadoresCampo(jugadoresCampo);
-}
-cargarJugadoresCampo().then(res=>console.log('Jugadores cargados')); */
+
+btnLista.addEventListener('click', mostarOcultarTablaJugadores);
